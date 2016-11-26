eb9e096edb410c516b133c38df37b46578b61e2c
Bruno-Torres <pipocaface123@gmail.com>
2016-11-26 11:50:12 +0000

    Game

--- Game/Assets/Player/PlayerController.cs
+++ Game/Assets/Player/PlayerController.cs
@@ -3,16 +3,47 @@ using UnityEngine.Networking;
 
 public class PlayerController : NetworkBehaviour
 {
+	public GameObject bulletPrefab;
+	public Transform bulletSpawn;
+
+	public GameObject cam;
+
     void Update()
     {
         if (!isLocalPlayer)
         {
             return;
         }
-        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 250.0f;
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 500.0f;
         var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
 
         transform.Rotate(0, x, 0);
         transform.Translate(0, 0, z);
+
+		if (Input.GetKeyDown (KeyCode.Space)) 
+		{
+			Fire ();
+		}
     }
+
+	void Fire ()
+	{
+		// Create the Bullet from the Bullet Prefab
+        var bullet = (GameObject)Instantiate(
+			bulletPrefab,
+			bulletSpawn.position,
+			bulletSpawn.rotation);
+
+		// Add velocity to the Bullet
+		bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 6;
+
+		// Destroy the bullet after 2 seconds
+		Destroy(bullet, 2.0f);
+	}
+
+	public override void OnStartLocalPlayer()
+	{
+		cam.SetActive (true);
+		GetComponent<MeshRenderer> ().material.color = Color.blue;
+	}
 }
